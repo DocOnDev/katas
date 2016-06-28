f7312b1fc131ca79883d67c3db2afb6cacabecab
Doc Norton <michael@docondev.com>
2016-06-28 08:34:33 -0400

    push old changes

--- string_calculator/spec/calc_spec.rb
+++ string_calculator/spec/calc_spec.rb
@@ -7,5 +7,13 @@ describe StringCalculator do
     it "returns 0 for an empty string" do
       expect(calc.add("")).to eq 0
     end
+
+    it "returns the value for a single digit string" do
+      expect(calc.add("2")).to eq 2
+    end
+
+    it "returns the sum for more than one digit" do
+      expect(calc.add("1,2")).to eq 3
+    end
   end
 end
